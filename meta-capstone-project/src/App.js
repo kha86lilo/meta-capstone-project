ad35ecd706232f97ebd21cb9a03615a479ac43e0
Your Name <you@example.com>
2023-12-10 02:16:04 +0300

    complete html

--- meta-capstone-project/src/App.js
+++ meta-capstone-project/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-     Homepage
-    </div>
+    <>
+    <Header></Header>
+    <Nav></Nav>
+    <Main></Main>
+    <Footer></Footer>
+    </>
   );
 }
 
---
+++ meta-capstone-project/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+    return
+    <footer>
+        <section> 
+            <p><a href="">Home</a></p>
+            <p><a href="">About</a></p>
+            <p><a href="">Menu</a></p>
+            <p><a href="">Reservation</a></p>
+            <p><a href="">Order Onpne</a></p>
+            <p><a href="">Login</a></p>
+        </section>
+        <section> 
+            <p><a href="">Address</a></p>
+            <p><a href="">Phone Number</a></p>
+            <p><a href="">Email</a></p> 
+        </section>
+        <section> 
+            <p><a href="">Facebook</a></p>
+            <p><a href="">Instagram</a></p>
+            <p><a href="">Twitter</a></p> 
+        </section>
+    </footer>
+}
+export default Footer;
